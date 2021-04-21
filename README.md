38deb44f56f94644d6427e64cd83f3bbb3b8f4d5
martin maguire <martin.maguire89@hotmail.co.uk>
2021-04-21 08:37:35 +0100

    user modal and express validation added

---
+++ README.md
@@ -0,0 +1,5 @@
+# Contact Keeper
+
+## express validator
+
+express valdiator used in users.js to allow us to validate what is put in for things like name, email and password. It allows us to only validate when something is entered correctly and not left empty.
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check'); //<< Lets us control what is entered and validated
+
+const User = require('../models/User');
 
 //@route        POST api/users
 // @desc        Register a user
 //@access       Public
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Please add name').not().isEmpty(), //<< This is so the filed is no left empty
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'please enater a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('passed');
+  }
+);
 
 module.exports = router;
